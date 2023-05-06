7dd60f57c7e26531cf536516adb6b6ba11fbc7d6
elijah <2818195805@qq.com>
2023-05-06 12:04:08 +0800

    add attention argument

--- generation/train.py
+++ generation/train.py
@@ -29,6 +29,7 @@ def get_args():
     parser.add_argument("--model-type",
                         default="transformer",
                         choices=["lstm", "transformer"])
+    parser.add_argument("--attention", default=True, action="store_true")
     args = parser.parse_args()
     return args
 
